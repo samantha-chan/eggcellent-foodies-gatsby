49b5f1d0d09c98033ebd53bf4529a89e8a4b971b
Samantha Chan <samanthachandev@gmail.com>
2021-06-19 02:10:52 -0400

    Add Image asset block to content

--- src/components/ContentBlock.jsx
+++ src/components/ContentBlock.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
 import { BLOCKS, MARKS } from "@contentful/rich-text-types"
 import { renderRichText } from 'gatsby-source-contentful/rich-text'
+import styled from 'styled-components'
 
+
+const Img = styled.img`
+  max-width: 100%;
+  height: 100%;
+`
 const Bold = ({ children }) => <strong>{children}</strong>
 const Paragraph = ({ children }) => <p>{children}</p>
+
 
 const options = {
   renderMark: {
@@ -14,22 +21,8 @@ const options = {
       return <Paragraph>{children}</Paragraph>
     },
     [BLOCKS.EMBEDDED_ASSET]: (node) => {
-      const imageID = node.data.target.sys.id
-      console.log("🚀 ~ file: ContentBlock.jsx ~ line 18 ~ imageID", imageID)
-    //   const {
-    //     file: {url}, 
-    //     title
-    //  } = props.data.contentfulBlogPost.body.references.find(({contentful_id: id}) => id === imageID);
-    //     console.log("🚀 ~ file: ContentBlock.jsx ~ line 23 ~ url", url)
-    
-      return (
-        <>
-          <h2>Embedded Asset</h2>
-          <pre>
-          {/* <img src={`https://${node.data.target.fields.file.url}`} /> */}
-          </pre>
-        </>
-      )
+    console.log("🚀 ~ file: ContentBlock.tsx ~ line 20 ~ node", node)
+      return <Img src={`https://${node.data.target.fluid.src}`} />
     }
   }
 }
